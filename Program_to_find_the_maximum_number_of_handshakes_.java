6022381b49ee3083a757652def2698eafccdbe36
tejaroykorukonda <97439402+tejaroykorukonda@users.noreply.github.com>
2022-09-09 11:58:58 +0530

    import java.util.Scanner; class handshks {    public static void main(String args[]) { int a; Scanner sc=new Scanner(System.in); a=sc.nextInt();
    System.out.println(a*(a-1)/2);
    sc.close();
    }
    }

---
+++ Program_to_find_the_maximum_number_of_handshakes_.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class handshks
+{
+   public static void main(String args[])
+{
+int a;
+Scanner sc=new Scanner(System.in);
+a=sc.nextInt();
+
+ 
+System.out.println(a*(a-1)/2);
+sc.close();
+}
+}
