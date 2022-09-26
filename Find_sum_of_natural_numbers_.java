81b17a7c58f82a77169abc7549e76ce3b20cd9bd
tejaroykorukonda <97439402+tejaroykorukonda@users.noreply.github.com>
2022-09-26 18:49:09 +0530

    import java.util.Scanner; class num{     public static void main(String[] args){         Scanner sc=new Scanner(System.in);         int a=sc.nextInt();         int k=(a)*(a+1)/2;         System.out.println(k);
        }
    }

---
+++ Find_sum_of_natural_numbers_.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class num{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int k=(a)*(a+1)/2;
+        System.out.println(k);
+        
+    }
+}
