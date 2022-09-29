d0123dfa5e2a703c6fea3f30fcbf87619f82cdb9
tejaroykorukonda <97439402+tejaroykorukonda@users.noreply.github.com>
2022-09-29 10:14:02 +0530

    import java.util.Scanner; class num {     public static void main(String arg[]){     int n,i,j,k;     Scanner sc=new Scanner(System.in);     n=sc.nextInt();     for(i=n;i>=1;i--)     {         for(j=1;j<i;j++)         {             System.out.print(" ");         }         for(k=1;k<=n;k++)         {             if(i==1||i==n||k==1||k==n)             {                  System.out.print("*");             }             else             {                  System.out.print(" ");             }         }          System.out.println();     } } }

--- print_the_following_pattern.java
+++ print_the_following_pattern.java
@@ -1,16 +1,28 @@
 import java.util.Scanner;
-class num{
+class num
+{
     public static void main(String arg[]){
-        Scanner sc=new Scanner(System.in);
-        int n,i,j;
-        n=sc.nextInt();
-        int alp=64;
-        for(i=n;i>=1;i--){
-            for(j=1;j<=i;j++){
-                System.out.print((char)(i+alp)+" ");
-            }
-            System.out.println();
+    int n,i,j,k;
+    Scanner sc=new Scanner(System.in);
+    n=sc.nextInt();
+    for(i=n;i>=1;i--)
+    {
+        for(j=1;j<i;j++)
+        {
+            System.out.print(" ");
         }
-        
+        for(k=1;k<=n;k++)
+        {
+            if(i==1||i==n||k==1||k==n)
+            {
+                 System.out.print("*");
+            }
+            else
+            {
+                 System.out.print(" ");
+            }
+        }
+         System.out.println();
     }
+}
 }
