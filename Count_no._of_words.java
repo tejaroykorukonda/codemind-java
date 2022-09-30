99d4cb6ce22a6756be16848af5cffb4b7f227d48
tejaroykorukonda <97439402+tejaroykorukonda@users.noreply.github.com>
2022-09-30 10:57:28 +0530

    import java.util.*; class num{     public static void main(String args[]){         Scanner sc=new Scanner(System.in);         String s1=sc.nextLine();         String s2[]=s1.split(" ");         System.out.println(s2.length);     } }

---
+++ Count_no._of_words.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class num{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        String s1=sc.nextLine();
+        String s2[]=s1.split(" ");
+        System.out.println(s2.length);
+    }
+}
